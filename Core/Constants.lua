f90d4bfb3a23a2dae61c4d68c6c86a7590d705dc
Vati <vatia0@gmail.com>
2026-03-29 17:38:24 +0400

    fix: added q item

--- Core/Constants.lua
+++ Core/Constants.lua
@@ -123,6 +123,7 @@ Constants.QUEST_INDICATOR_IGNORE = {
 -- These items are usable quest-related items that should appear in the quest bar
 Constants.CUSTOM_QUEST_ITEMS = {
     [24287] = true,  -- Extinguishing Mixture
+    [24501] = true,  -- Gordawg's Boulder
     [28038] = true,  -- Seaforium PU-36 Explosive Nether Modulator
     [28132] = true,  -- Area 52 Special
     [25465] = true,  -- Stormcrow Amulet
